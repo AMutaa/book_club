dfd83462433def1fce4bd2253c8e3bca8aa30934
AMutaa <piusandru@gmail.com>
2019-11-15 15:37:08 -0500

    update

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,42 +1,48 @@
-import React, { Component } from 'react';
-import styled from 'styled-components'
-import { Header, Content, Footer } from 'Components'
-import { Card, Button } from 'Elements'
-import { colors } from 'Utilities';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Header, Content, Footer } from "Components";
+import { Card, Button } from "Elements";
+import { colors } from "Utilities";
+import Select from "@blueprintjs/core";
 
 export default class Form extends Component {
   render() {
     return (
-      <Card type='signin'>
-        <Header type='signin'>
+      <Card type="signin">
+        <Header type="signin">
           <h1>SIGN IN</h1>
         </Header>
         <Content>
           <form action="">
             <p>Username</p>
-            <Input type='text' name='firstname' required />
-            <div style={{ marginTop: '20px' }}></div>
+            <Input type="text" name="firstname" required />
+            <div style={{ marginTop: "20px" }}></div>
             <p>Password</p>
-            <Input type='text' name='lastname' required />
-            <Footer><Button modifiers='signin' type='submit'>SIGN IN</Button></Footer>
+            <Input type="text" name="lastname" required />
+            <Footer>
+              <Button modifiers="signin" type="submit">
+                SIGN IN
+              </Button>
+            </Footer>
           </form>
         </Content>
         <Footer>
-          <h5><span>Not a member ?</span> Sign Up Now </h5>
+          <h5>
+            <span>Not a member ?</span> Sign Up Now{" "}
+          </h5>
         </Footer>
-      </Card >
-    )
+      </Card>
+    );
   }
 }
 const Input = styled.input`
-height: 2.5rem;
-width: 90%;
-color: ${colors.white};
-padding-left: 5px;
-outline: none;
-font-size: 1rem;
-border: none;
-border-radius: 4px;
-background: ${colors.darkGrey}
-
-`
+  height: 2.5rem;
+  width: 90%;
+  color: ${colors.white};
+  padding-left: 5px;
+  outline: none;
+  font-size: 1rem;
+  border: none;
+  border-radius: 4px;
+  background: ${colors.darkGrey};
+`;
